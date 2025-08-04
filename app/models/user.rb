e40eb0435baaefa479d139769509e4b896e91427
Hamza Ahmad <jhaji1223@gmail.com>
2025-08-03 20:10:35 -0700

    syntax clear

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,10 @@
 class User < ApplicationRecord
-  # enums
-
+  
   enum role: { user: 0, developer: 1, qa: 2, admin: 3 }
-
-  # associations
 
   has_many :created_tickets, foreign_key: "creator"
   has_many :developed_tickets, foreign_key: "developer"
   has_many :qa_tickets, foreign_key: "qa"
-
-  # Validations
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
@@ -17,7 +12,6 @@ class User < ApplicationRecord
   validate :email_is_in_correct_format
 
   private
-
   def email_is_in_correct_format
     email_string = self.email.to_s.strip
     if email_string.count("@") !=1
