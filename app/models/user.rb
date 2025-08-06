0a98af46fc5b440942709404a3eb8e4274cf1adb
Hamza Ahmad <jhaji1223@gmail.com>
2025-08-06 03:46:04 -0700

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
-
  after_initialize :set_default_role, if: :new_record?
 
   # Include default devise modules. Others available are:
   #  :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :confirmable,:recoverable , :rememberable, :validatable
+         :confirmable, :recoverable, :rememberable, :validatable
 
   enum role: { user: 0, developer: 1, qa: 2, admin: 3 }
 
